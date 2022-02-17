db10d484577fc861ce1d03de4acd58266dac3b7c
James Holland <jholland4134@floridapoly.edu>
2022-02-17 13:54:37 -0600

    changes

--- remoteControl/esp32_remote/src/main.cpp
+++ remoteControl/esp32_remote/src/main.cpp
@@ -1,4 +1,12 @@
 #include <Arduino.h>
+#include "BluetoothSerial.h"
+
+// Bluetooth
+#if !defined(CONFIG_BT_ENABLED) || !defined(CONFIG_BLUEDROID_ENABLED)
+#error Bluetooth is not enabled! Please run `make menuconfig` to and enable it
+#endif
+
+BluetoothSerial SerialBT;
 
 // Define joystick pins
 int back = 2;
@@ -16,9 +24,14 @@ bool left_bwd = false;
 bool right_fwd = false;
 bool right_bwd = false;
 
+// Message formatting
+int left_dir = 0;
+int right_dir = 0;
 int speed = 0;
 
 void setup() {
+  Serial.println("gantryOS Version 1 Initializing");
+  Serial.print(" . ");Serial.print(" . ");Serial.println(" . ");
   // Set input pins mode
   // Joystick
   pinMode(forward,INPUT_PULLUP); pinMode(back,INPUT_PULLUP); pinMode(right,INPUT_PULLUP); pinMode(left,INPUT_PULLUP);  
@@ -29,6 +42,8 @@ void setup() {
   // Set up bluetooth or wifi connection
   Serial.begin(115200);
 
+  SerialBT.begin("gantryController"); //Bluetooth device name
+  Serial.println("System Ready. Enjoy! :D\n\n\n\n\n\n\n\n");
 }
 
 void loop() {
@@ -36,32 +51,38 @@ void loop() {
   // Joystick
   if(digitalRead(forward) == false){
     left_fwd = true; left_bwd = false;
-    Serial.println("fwd");
+    // Serial.println("fwd");
     sleep(1);
   } 
   else if(digitalRead(back) == false){
     left_fwd = false; left_bwd = true;
-    Serial.println("bwd");
+    // Serial.println("bwd");
     sleep(1);
   }
   if(digitalRead(right) == false){
     right_fwd = true; right_bwd = false;
-    Serial.println("right");
+    // Serial.println("right");
     sleep(1);
   }
   else if(digitalRead(left) == false){
     right_fwd = false; right_bwd = true;
-    Serial.println("left");
+    // Serial.println("left");
     sleep(1);
   }
 
   // Buttons
-  if(digitalRead(a) == true){speed+=1; Serial.println(speed); sleep(1);} // Increase speed
-  if(digitalRead(b) == true){speed-=1; Serial.println(speed); sleep(1);} // Decrease speed
+  if(digitalRead(a) == true){speed+=1; sleep(1);} // Increase speed
+  if(digitalRead(b) == true){speed-=1; sleep(1);} // Decrease speed
   
   // Print to serial monitor
+  // Serial.println("Speed: "+String(speed));
 
   // Transmit to RPI
+  if (SerialBT.available()) {
+    Serial.write(left_dir+right_dir+speed);
+  }
+
+  Serial.println(left_dir+right_dir+speed);
 
   // sleep(1);
 }
